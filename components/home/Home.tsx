fdc6c268b73ae312ebf96e81b593cf2f09b6d6ca
abdulbariks <abdulbarik1997m@gmail.com>
2026-05-23 15:17:44 +0600

    Experience, Projects, and Footer Created

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import About from './About'
+import Experience from './Experience'
+import Projects from './Projects'
 
 export default function Home() {
   return (
     <div className="space-y-10 sm:space-y-16">
       <HeroSection />
-      {/* <About />
+      <About />
       <Experience />
-      <Projects /> */}
+      <Projects />
     </div>
   )
 }
